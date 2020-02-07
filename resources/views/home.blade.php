ad53c1ca4b67032c083f87db96a21be89dfc9412
zero <mariobigboy@gmail.com>
2020-02-07 20:51:33 -0300

    provisionalmente agrego estilo y creo el home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -80,7 +80,23 @@
 		</div>
 	</nav>
 
-	
+	<section id="gtco-hero" class="gtco-cover" style="background-image: url(images/img_bg_4.jpg);"  data-section="home"  data-stellar-background-ratio="0.5">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 col-md-offset-0 text-center">
+					<div class="display-t">
+						<div class="display-tc">
+							<h1 class="animate-box" data-animate-effect="fadeIn">Lo pensamos para vos <br>
+                            lo vivimos con vos</h1>
+                            <h3 style="color: #FFF">Buenos Aires 2020</h3>
+							<p class="gtco-video-link animate-box" data-animate-effect="fadeIn"><a href="https://vimeo.com/channels/staffpicks/93951774" class="popup-vimeo"><i class="icon-controller-play"></i></a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 	
 	<section id="gtco-about" data-section="about">
 		<div class="container">
