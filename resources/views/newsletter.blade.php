22d5dc1c3e11066a91a3df3dc479a2df73de1201
Paline <mendezpablo90@gmail.com>
2020-03-04 14:32:27 -0300

    modificada imagenes hoteles

--- resources/views/newsletter.blade.php
+++ resources/views/newsletter.blade.php
@@ -2,11 +2,21 @@
 <div class="single-widget-area mb-100">
     <div class="newsletter-form">
         <h5>Newsletter</h5>
-        <p>Subscribe our newsletter gor get notification new updates.</p>
+        <p>Subscríbase a nuestro newsletter para obtener noticias y actualizaciones.</p>
 
         <form action="#" method="post">
-            <input type="email" name="nl-email" id="nlEmail" class="form-control" placeholder="Enter your email...">
-            <button type="submit" class="btn roberto-btn w-100">Subscribe</button>
+            <input type="email" name="nl-email" id="nlEmail" class="form-control" placeholder="Ingrese email...">
+            <button type="submit" class="btn roberto-btn w-100">Subscribir</button>
         </form>
+        <br>
+        @if(session('success_newsletter'))
+        <div class="alert alert-success">
+	      <button type="button" aria-hidden="true" class="close" data-dismiss="alert" aria-label="Close">
+	        <i class="tim-icons icon-simple-remove"></i>
+	      </button>
+	      <span>¡Subscripto correctamente!</span>
+	    </div>
+	    @endif
     </div>
+
 </div>
